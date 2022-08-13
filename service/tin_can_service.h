3216ab8e74a6b02e351d563e0aa26d4bec360f1f
Eddie <eddie@edwinfairchild.com>
2022-08-13 13:44:42 -0500

    UUID building fixed and commented

--- service/tin_can_service.h
+++ service/tin_can_service.h
@@ -8,12 +8,19 @@
 #include "util/bstream.h"
 #include "att_api.h"
 
-#define TIN_CAN_SERVICE_UUID 0xBE, 0xC5, 0xD1, 0x24, 0x99, 0x33, 0xC6, 0x87, 0x85, 0x41, 0xD9, 0x31, 0x7D, 0x56, 0xFC, 0x85 
+#define TIN_CAN_SERVICE_UUID_BASE 0xBE, 0xC5, 0xD1, 0x24, 0x99, 0x33, 0xC6, 0x87, 0x85, 0x41, 0xD9, 0x31, 0x7D, 0x56
+#define ATT_UUID_TIN_CAN_BUILD(part)            UINT16_TO_BYTES(part), TIN_CAN_SERVICE_UUID_BASE
 
-#define BUTTON_VALUE_CHART_PARTIAL 0x123
+
+#define BUTTON_VALUE_CHART_PARTIAL 0x1230
+#define  TIN_CAN_SERVICE_PARTIAL 0xfc85
 /* Builds UUIDS from ARM base UUID and a unique partial */
-#define BUTTON_VALUE_CHART_UUID   ATT_UUID_ARM_BUILD(0x100)
+#define TIN_CAN_SERVICE_UUID      ATT_UUID_TIN_CAN_BUILD(TIN_CAN_SERVICE_PARTIAL)
+#define BUTTON_VALUE_CHART_UUID   ATT_UUID_TIN_CAN_BUILD(BUTTON_VALUE_CHART_PARTIAL)
 
+/* the partial gets turn into little endian and then appended to beggning of the base
+   the entire UUID is then advertised in little endian format also, in other words the reverse order
+   that is seen above */
 
 #define START_HDL 0x1000 /* arbitrary numbver chosen */
 /* Characteristic Handles */
